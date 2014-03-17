8e3297f73ad42f7c125d095bcef5d836b9aaa6a0
William Delanoue <william.delanoue@deveryware.net>
2014-03-17 18:19:55 +0100

    add vim

--- restore_links.sh
+++ restore_links.sh
@@ -17,5 +17,11 @@ ln -s "$PWD/.screenrc" $HOME
 
 if [ ! -d ~/.oh-my-zsh ] ;
 then
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+if [ ! -d ~/.vim ] ;
+then
+  git clone git@github.com:twillouer/vim-config.git ~/.vim
+  ln -s ~/.vim/vimrc ~/.vimrc
 fi
